f0e5cfeec20cf878ecec43cd3496c965fee90cd4
t <921865806@qq.com>
2026-07-06 18:22:28 +0800

    修改脚本

--- deploy/build-game.sh
+++ deploy/build-game.sh
@@ -3,7 +3,7 @@
  # @Author: t 921865806@qq.com
  # @Date: 2026-01-03 21:43:05
  # @LastEditors: t 921865806@qq.com
- # @LastEditTime: 2026-01-16 16:02:13
+ # @LastEditTime: 2026-07-05 20:48:59
  # @FilePath: /examples/deploy/build-game.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
@@ -23,7 +23,8 @@ cd "$PROJECT_ROOT"
 
 # 构建
 echo "📦 编译中..."
-go build -mod=vendor -ldflags="-s -w" -o "$OUTPUT" ./demo_cluster/nodes/main.go
+# go build -ldflags="-s -w" -o "$OUTPUT" ./demo_cluster/nodes/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o "$OUTPUT" ./demo_cluster/nodes/main.go
 
 echo ""
 echo "✅ 构建完成: $OUTPUT"
